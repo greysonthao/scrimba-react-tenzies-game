82176726f76cc7663ad1d37f92a177d801e1e4fa
Touger Thao <tougerthao@gmail.com>
2022-03-30 11:30:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Die from "../src/components/Die";
 
 function App() {
+  const [allDice, setAllDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      let randNum = Math.ceil(Math.random() * 6);
+      newDice.push(randNum);
+    }
+    return newDice;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main-container">
+      <div className="die-container">
+        <Die value={allDice[0]} />
+        <Die value={allDice[1]} />
+        <Die value={allDice[2]} />
+        <Die value={allDice[3]} />
+        <Die value={allDice[4]} />
+        <Die value={allDice[5]} />
+        <Die value={allDice[6]} />
+        <Die value={allDice[7]} />
+        <Die value={allDice[8]} />
+        <Die value={allDice[9]} />
+      </div>
+    </main>
   );
 }
 
